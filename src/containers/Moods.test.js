dda9d3f407bd25df22f1d334fcc0d54a98a1b417
Travis Molter <travismolter@gmail.com>
2020-02-13 16:53:56 -0800

    snapshot actually working now

--- src/containers/Moods.test.js
+++ src/containers/Moods.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import Moods from './Moods';
 import store from '../store';
 describe('container', () => {
   it('matches a snapshot', () => {
-    const wrapper = shallow(<Provider store={store}><Moods /></Provider>);
+    const wrapper = mount(<Provider store={store}><Moods /></Provider>);
     expect(wrapper).toMatchSnapshot();
   });
 });
