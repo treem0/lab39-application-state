4083a911fa24f05f8aad77d7af2d6bbd6315b4df
Travis Molter <travismolter@gmail.com>
2020-02-13 16:18:00 -0800

    added the state back to the buttons to tell how many times it is pushed

--- src/containers/Moods.js
+++ src/containers/Moods.js
@@ -9,14 +9,18 @@ import { getFace } from '../selectors/emojiSelector';
 const Moods = () => {
   const dispatch = useDispatch();
   const emojiFace = useSelector(getFace);
+  const coffees = useSelector(state => state.coffees);
+  const snacks = useSelector(state => state.snacks);
+  const naps = useSelector(state => state.naps);
+  const studies = useSelector(state => state.studies);
 
   return (
     <>
       <Controls>
-        <button onClick={() => dispatch(drinkCoffee())}>coffee</button>
-        <button onClick={() => dispatch(eatSnack())}>snacks</button>
-        <button onClick={() => dispatch(takeNap())}>naps</button>
-        <button onClick={() => dispatch(study())}>studies</button>
+        <button onClick={() => dispatch(drinkCoffee())}>coffee - {coffees}</button>
+        <button onClick={() => dispatch(eatSnack())}>snacks - {snacks}</button>
+        <button onClick={() => dispatch(takeNap())}>naps - {naps}</button>
+        <button onClick={() => dispatch(study())}>studies - {studies}</button>
       </Controls>
       <Face emoji={emojiFace} />
     </>
